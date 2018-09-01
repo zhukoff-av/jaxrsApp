ac063c848f94372adb7e34a10b14ed528476cabb
zhukoff-av <mr.zhukoff@gmail.com>
2018-09-01 20:51:38 +0200

    add service

---
+++ src/main/java/com/az/rest/JSONService.java
@@ -0,0 +1,39 @@
+package com.az.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.az.Track;
+
+@Path("/json/linkinPark")
+public class JSONService {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Track getTrackInJSON() {
+
+		Track track = new Track();
+		track.setTitle("Crawling");
+		track.setSinger("Linkin Park");
+
+		return track;
+
+	}
+
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Track track) {
+
+		String result = "Track saved : " + track;
+		return Response.status(201).entity(result).build();
+		
+	}
+	
+}
